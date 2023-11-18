68f9ce8b4d56f189489b09c73bf41e67bd26f92e
Daniel Pang <dpang@Daniels-MBP-3.localdomain>
2023-11-18 13:42:39 -0800

    show the query

--- first.py
+++ first.py
@@ -21,13 +21,15 @@ def get_ai_snippets_for_query(query):
 #    headers = {"X-API-Key": "5447f8dd-ba5a-406a-944a-d1ba07b0002e<__>1ODmHdETU8N2v5f4K7rDr9X0"}
     headers = {"X-API-Key": YOU_API_KEY}
     params = {"query": query,
-		"num_web_resuts":3}
+		"num_web_results":3}
     return requests.get(
         f"https://api.ydc-index.io/search?query={query}",
         params=params,
         headers=headers,
     ).json()
 
+query="Find me a computer monitor that is at least 34 inches across and is a good value.  Give me shopping links to Amazon.com"
+print("Query: "+query)
 # results = get_ai_snippets_for_query("Name three news items of note from November 17, 2023")
-results = get_ai_snippets_for_query("Find me a computer monitor that is at least 34 inches across and is a good value.  Give me shopping links to Amazon.com")
+results = get_ai_snippets_for_query(query)
 print_list_elements(results['hits'])
